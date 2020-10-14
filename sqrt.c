3f34795b9b3aa108d84cd2e590e4fd4a1cf98e1b
snowsanta <45097716+snowsanta@users.noreply.github.com>
2020-10-14 10:56:40 +0530

    negetive support

--- sqrt.c
+++ sqrt.c
@@ -4,6 +4,8 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
 
 // added first comment
 int main(int argc, char *argv[])
@@ -16,17 +18,41 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
+	// input validation
+	int length = strlen(argv[1]);
+	int i = 0;
+	if (argv[1][0] == '-')
+	{
+		i++;
+	}
+	for (i; i < length; i++)
+	{
+		if (!isdigit(argv[1][i]) && argv)
+		{
+			printf("Error: Input must be a integer\n\n");
+			exit(1);
+		}
+	}
+
 	// if given valid input, calculate and print square root
 	int input = atoi(argv[1]);
 
-	// input validation
+	//handle negetive input
+	int flg = 0;
 	if (input < 0)
 	{
-		printf("Error: Input must be non negetive integer\n\n");
-		exit(-1);
+		flg = 1;
+		input = input * -1;
 	}
 
-	printf("Sqrt of %d is %f\n", input, sqrt(input));
+	if (flg == 1)
+	{
+		printf("Sqrt of %d is %fi\n", input * -1, sqrt(input));
+	}
+	else
+	{
+		printf("Sqrt of %d is %f\n", input, sqrt(input));
+	}
 	printf("End of program, exiting\n");
 	return (0);
 
